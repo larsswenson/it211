3af0cdef57dfa1b470bb685b4e07d7ef76bb6017
larswenson <larsswenson@seattlecolleges.edu>
2023-01-23 19:09:15 -0800

    uploads converter

---
+++ LitersToGallons.java
@@ -0,0 +1,13 @@
+public class LitersToGallons {
+    public static void main(String[] args) {
+        double liters;
+        double gallons;
+
+        liters = 6.0;
+
+        gallons = 0.264172 * liters;
+
+        System.out.println(liters + " liters is " + gallons + " gallons");
+
+    }
+}
